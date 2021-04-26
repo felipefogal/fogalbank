a80d6e32a29a2c65cf24b4bdfdae3fa1d4e6537a
Felipe Garcia Fogal <felipe.fogal@gruposbf.com.br>
2021-04-26 17:58:36 -0300

    Criando primeiras variáveis para os dados de conta corrente

--- fogalbank/Program.cs
+++ fogalbank/Program.cs
@@ -6,6 +6,10 @@ namespace fogalbank
     {
         static void Main(string[] args)
         {
+            public string titularDaConta = "Alessandra";
+            public int numeroDaAgencia = 0032;
+            public int numeroDaConta = 123456;
+            public double saldoDaConta = 13.00;
         }
     }
 }

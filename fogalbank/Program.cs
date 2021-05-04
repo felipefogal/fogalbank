7d1289286e8beb135133a200561d9eccbd7a7fb1
Taynara Simoes <taynara.simoes@gruposbf.com.br>
2021-05-04 17:57:50 -0300

    Inserindo classe Conta Corrente

--- fogalbank/Program.cs
+++ fogalbank/Program.cs
@@ -6,10 +6,22 @@ namespace fogalbank
     {
         static void Main(string[] args)
         {
-            public string titularDaConta = "Fogal";
-            public int numeroDaAgencia = 0032;
-            public int numeroDaConta = 123456;
-            public double saldoDaConta = 13.00;
+            ContaCorrente cc = new ContaCorrente();
+            cc.titularDaConta = "Tay";
+            cc.numeroDaAgencia = 1234;
+            cc.numeroDaConta = 567890;
+            cc.saldoDaConta = 10;
+
+            ContaCorrente cc2 = new ContaCorrente();
+            cc2.titularDaConta = "Fogal";
+            cc2.numeroDaAgencia = 1234;
+            cc2.numeroDaConta = 123409;
+            cc2.saldoDaConta = 20;
+
+            Console.WriteLine("O nome do titular é " + cc.titularDaConta);
+            Console.WriteLine("O nome do titular da segunda conta é " + cc2.titularDaConta);
+
+
         }
     }
 }
